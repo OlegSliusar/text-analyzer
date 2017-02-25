4419327954ce1b52d3158ced549f82fd119e5f21
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-25 13:56:00 +0200

    Add paragraphs counting feature

--- analyzer.rb
+++ analyzer.rb
@@ -5,8 +5,11 @@ total_characters = text.length
 total_characters_nospaces = text.gsub(/\s+/, '').length
 total_words = text.split.length
 sentence_count = text.split(/\.|\?|!/).length
+paragraph_count = text.split(/\n\n/).length
+
 puts "#{total_characters} characters"
 puts "#{total_characters_nospaces} characters (excluding spaces)"
 puts "#{line_count} lines"
 puts "#{total_words} words"
 puts "#{sentence_count} sentences"
+puts "#{paragraph_count} paragraphs"
