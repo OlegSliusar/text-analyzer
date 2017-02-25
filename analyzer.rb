0b18ae643d1231a1c950226936c11916c79a9352
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-25 13:40:52 +0200

    Add statistic of number of words

--- analyzer.rb
+++ analyzer.rb
@@ -3,6 +3,8 @@ line_count = lines.size
 text = lines.join
 total_characters = text.length
 total_characters_nospaces = text.gsub(/\s+/, '').length
+total_words = text.split.length
 puts "#{total_characters} characters"
 puts "#{total_characters_nospaces} characters (excluding spaces)"
 puts "#{line_count} lines"
+puts "#{total_words} words"
