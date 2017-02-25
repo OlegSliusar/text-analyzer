36e5fdfe8aaad63686185d169523338e9df2ab55
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-25 14:08:17 +0200

    Add statistics on averages

--- analyzer.rb
+++ analyzer.rb
@@ -1,15 +1,17 @@
 lines = File.readlines("text.txt")
 line_count = lines.size
 text = lines.join
-total_characters = text.length
-total_characters_nospaces = text.gsub(/\s+/, '').length
-total_words = text.split.length
+character_count = text.length
+character_count_nospaces = text.gsub(/\s+/, '').length
+word_count = text.split.length
 sentence_count = text.split(/\.|\?|!/).length
 paragraph_count = text.split(/\n\n/).length
 
-puts "#{total_characters} characters"
-puts "#{total_characters_nospaces} characters (excluding spaces)"
+puts "#{character_count} characters"
+puts "#{character_count_nospaces} characters (excluding spaces)"
 puts "#{line_count} lines"
-puts "#{total_words} words"
+puts "#{word_count} words"
 puts "#{sentence_count} sentences"
 puts "#{paragraph_count} paragraphs"
+puts "#{word_count / sentence_count} words per sentence (average)"
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
