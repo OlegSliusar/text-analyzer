f8eeb2bba718c7124a1602713b4da2a637372fe8
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-25 13:07:48 +0200

    Add statistic of number of characters excluding spaces

--- analyzer.rb
+++ analyzer.rb
@@ -2,5 +2,7 @@ lines = File.readlines("text.txt")
 line_count = lines.size
 text = lines.join
 total_characters = text.length
+total_characters_nospaces = text.gsub(/\s+/, '').length
 puts "#{total_characters} characters"
+puts "#{total_characters_nospaces} characters (excluding spaces)"
 puts "#{line_count} lines"
